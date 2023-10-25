f148508c1c9c615e387232f802e247a1ed168c77
Gupta <amangup@apac.corpdir.net>
2023-10-25 23:51:35 +0530

    aws sns subs

---
+++ functions/sns-subscription.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+    const { SubscribeURL } = JSON.parse(event.body);
+
+    if (SubscribeURL) {
+        try {
+            // Send an HTTP GET request to the SubscribeURL to confirm the subscription
+            const response = await axios.get(SubscribeURL, { timeout: 10000 });
+
+            if (response.status === 200) {
+                // Subscription is confirmed
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Subscription confirmed' })
+                };
+            } else {
+                // Handle non-200 responses as needed
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ message: 'Failed to confirm subscription' })
+                };
+            }
+        } catch (error) {
+            // Handle request errors
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Error confirming subscription' })
+            };
+        }
+    } else {
+        // Handle other SNS notifications here
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Received SNS notification' })
+        };
+    }
+};
