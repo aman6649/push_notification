16945fd5ad5306a9904599f90f7e4377478175d3
Gupta <amangup@apac.corpdir.net>
2023-10-26 11:30:57 +0530

    endpoint changed

--- netlify/functions/sns-subscription.js
+++ netlify/functions/sns-subscription.js
@@ -1,60 +1,60 @@
-exports.handler = async function(event, context) {
-    try {
-        const data = JSON.parse(event.body);
-        // Process the data
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message: 'Success' })
-        };
-    } catch (error) {
-        console.error('Error in the function:', error);
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Internal Server Error' })
-        };
-    }
-};
-
-
-
-
-// const axios = require('axios');
-
 // exports.handler = async function(event, context) {
-//     const { SubscribeURL } = JSON.parse(event.body);
-//     console.log("subu"+SubscribeURL);
-//     if (SubscribeURL) {
-//         try {
-//             // Send an HTTP GET request to the SubscribeURL to confirm the subscription
-//             const response = await axios.get(SubscribeURL, { timeout: 10000 });
+//     try {
+//         const data = JSON.parse(event.body);
+//         // Process the data
 
-//             if (response.status === 200) {
-//                 // Subscription is confirmed
-//                 return {
-//                     statusCode: 200,
-//                     body: JSON.stringify({ message: 'Subscription confirmed' })
-//                 };
-//             } else {
-//                 // Handle non-200 responses as needed
-//                 return {
-//                     statusCode: 400,
-//                     body: JSON.stringify({ message: 'Failed to confirm subscription' })
-//                 };
-//             }
-//         } catch (error) {
-//             // Handle request errors
-//             return {
-//                 statusCode: 500,
-//                 body: JSON.stringify({ message: 'Error confirming subscription' })
-//             };
-//         }
-//     } else {
-//         // Handle other SNS notifications here
 //         return {
 //             statusCode: 200,
-//             body: JSON.stringify({ message: 'Received SNS notification' })
+//             body: JSON.stringify({ message: 'Success' })
+//         };
+//     } catch (error) {
+//         console.error('Error in the function:', error);
+
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ message: 'Internal Server Error' })
 //         };
 //     }
 // };
+
+
+
+
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+    const { SubscribeURL } = JSON.parse(event.body);
+    console.log("subu"+SubscribeURL);
+    if (SubscribeURL) {
+        try {
+            // Send an HTTP GET request to the SubscribeURL to confirm the subscription
+            const response = await axios.get(SubscribeURL, { timeout: 10000 });
+
+            if (response.status === 200) {
+                // Subscription is confirmed
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify({ message: 'Subscription confirmed' })
+                };
+            } else {
+                // Handle non-200 responses as needed
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ message: 'Failed to confirm subscription' })
+                };
+            }
+        } catch (error) {
+            // Handle request errors
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: 'Error confirming subscription' })
+            };
+        }
+    } else {
+        // Handle other SNS notifications here
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'Received SNS notification' })
+        };
+    }
+};
