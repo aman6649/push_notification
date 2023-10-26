db4e9df7f1067753eac4fe8bde85db2357fa322d
Gupta <amangup@apac.corpdir.net>
2023-10-26 13:50:18 +0530

    hi

--- netlify/functions/sns-subscription.js
+++ netlify/functions/sns-subscription.js
@@ -23,8 +23,9 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
+    console.log(event);
     const { SubscribeURL } = JSON.parse(event.body);
-    console.log("subu"+SubscribeURL);
+    console.log(SubscribeURL);
     if (SubscribeURL) {
         try {
             // Send an HTTP GET request to the SubscribeURL to confirm the subscription
